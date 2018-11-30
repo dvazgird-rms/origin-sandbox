277bbc6b5b058078feb40bca354cb4035fb08393
Damir Vazgird <Damir.Vazgird@rms.com>
2018-11-30 13:35:06 -0800

    linked radius locally - theme provider gets theme data from radius successfully

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,19 +1,24 @@
 import React from "react";
+import { ThemeProvider } from "styled-components";
 
-import { ThemeProvider, Button } from "origin-react";
+// import { ThemeProvider, Button } from "origin-react";
+import getConfig from "radius";
+import generateLoader from "radius";
 
 function App() {
+  console.log("get config", getConfig);
+  console.log("generate loader", generateLoader);
   return (
     <div>
-      <h1>Origin Sandbox</h1>
-      <p>Origin Button wrapped with ThemeProvider (default props)</p>
+      <h1>Radius/Origin Sandbox</h1>
+      {/* <p>Origin Button wrapped with ThemeProvider (default props)</p>
       <ThemeProvider>
         <Button label="Click Me" />
       </ThemeProvider>
       <p>Origin Button without ThemeProvider</p>
-      <Button label="Click Me" />
-      <p>Non-Origin Button wrapped with ThemeProvider</p>
-      <ThemeProvider>
+      <Button label="Click Me" /> */}
+      <p>button wrapped with ThemeProvider</p>
+      <ThemeProvider theme={generateLoader}>
         <button>Click Me!</button>
       </ThemeProvider>
     </div>
